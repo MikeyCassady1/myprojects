0d98bea66b83b79db578b7a7003a9e59678b495d
MikeyCassady1 <105388019+MikeyCassady1@users.noreply.github.com>
2022-05-11 14:43:08 -0500

    Add files via upload

---
+++ functions/functions/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  functions
+//
+//  Created by period8 on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func favoriteCeral() -> String {
+        return "Cinnamon Toast Crunch"
+    }
+
+    func favoriteSport(team:String, sport:String, city:String, players:Int)  {
+        var sentence = "My favorite sports team is " + team + "in the" + city + "!."
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
